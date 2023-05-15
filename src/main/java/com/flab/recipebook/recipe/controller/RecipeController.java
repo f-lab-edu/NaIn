2b8791f79900c700861b8ca902fb1fe9ebe76887
buchonsi <buchonsi@naver.com>
2023-05-15 19:04:15 +0900

    [#7] RecipeController 추가
    * 저장, ID로 recipe 찾기, title로 recipe 찾기 구현

--- src/main/java/com/flab/recipebook/recipe/controller/RecipeController.java
+++ src/main/java/com/flab/recipebook/recipe/controller/RecipeController.java
@@ -1,4 +1,34 @@
 package com.flab.recipebook.recipe.controller;
 
+import com.flab.recipebook.common.dto.ResponseResult;
+import com.flab.recipebook.recipe.dto.SaveRecipeDto;
+import com.flab.recipebook.recipe.service.RecipeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @PostMapping("/recipe/save")
+    public ResponseEntity<ResponseResult> saveRecipe(@RequestBody SaveRecipeDto saveRecipeDto) {
+        recipeService.saveRecipe(saveRecipeDto);
+        return new ResponseEntity(new ResponseResult(), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/recipe/{recipeId}")
+    public ResponseEntity<ResponseResult> findById(@PathVariable Long recipeId) {
+        return new ResponseEntity<>(new ResponseResult(recipeService.findById(recipeId)), HttpStatus.OK);
+    }
+
+    @GetMapping("/recipe/search/{title}")
+    public ResponseEntity<ResponseResult> findByTitle(@PathVariable String title) {
+        return new ResponseEntity<>(new ResponseResult(recipeService.findByTitle(title)), HttpStatus.OK);
+    }
 }
