c4f6db76dff212ddd411b01716308cdedae2033e
buchonsi <buchonsi@naver.com>
2023-05-15 19:10:01 +0900

    [#7] 주석 정리

--- src/main/java/com/flab/recipebook/recipe/service/RecipeService.java
+++ src/main/java/com/flab/recipebook/recipe/service/RecipeService.java
@@ -23,9 +23,11 @@ public class RecipeService {
         this.recipeDao = recipeDao;
     }
 
-    //레시피를 등록한다
-    //레시피 등록은 권한인 CHEF인 회원만 등록할 수 있다.
-    //레시피에는 제목, 재료, 내용을 작성할 수 있다.
+    /**
+     * 레시피를 등록한다
+     * 레시피 등록은 권한인 CHEF인 회원만 등록할 수 있다. (security 설정 추가)
+     * 레시피에는 제목, 재료, 내용을 작성할 수 있다.
+     */
     public void saveRecipe(SaveRecipeDto saveRecipeDto) {
         //권한 검증
         //recipe 변환
@@ -34,9 +36,11 @@ public class RecipeService {
         recipeDao.save(recipe);
     }
 
-    //레시피를 조회한다.
-    //레시피는 기본적으로 최근 등록순으로 정렬되어서 조회 된다.
-    //레시피 메인 검색은 제목에 포함된 문자열을 찾아 조회 된다.
+    /**
+     * 레시피를 조회한다.
+     * 레시피는 기본적으로 최근 등록순으로 정렬되어서 조회 된다.
+     * 레시피 메인 검색은 제목에 포함된 문자열을 찾아 조회 된다.
+     */
     public List<ResponseRecipeDto> findByTitle(String title) {
         return recipeDao.findByTitle(title).stream().map(
                 recipe -> {
@@ -44,10 +48,12 @@ public class RecipeService {
                 }).collect(Collectors.toList());
     }
 
-    //레시피 상세
-    //검색된 레시피를 클릭하면 상세한 내용을 볼 수 있다.
-    //레시피 상세 조회를 하면 댓글을 남길 수 있다.
-    //레시피 상세 조회를 하면 별점을 남길 수 있다.
+    /**
+     * 레시피 상세
+     * 검색된 레시피를 클릭하면 상세한 내용을 볼 수 있다.
+     * 레시피 상세 조회를 하면 댓글을 남길 수 있다. (댓글기능 추가 후)
+     * 레시피 상세 조회를 하면 별점을 남길 수 있다. (별점기능 추가 후)
+     */
     public ResponseRecipeDto findById(Long id) {
         Recipe recipe = recipeDao.findById(id).orElseThrow(RecipeNotFoundException::new);
         return convertResponseRecipe(recipe);
