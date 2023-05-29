02a284443970121cfcaaa1d77730edc7d182990b
buchonsi <buchonsi@naver.com>
2023-05-29 17:28:52 +0900

    [#7] 예외 처리 반복되는 코드 리팩토링

--- src/main/java/com/flab/recipebook/common/advice/ExceptionAdvice.java
+++ src/main/java/com/flab/recipebook/common/advice/ExceptionAdvice.java
@@ -1,8 +1,10 @@
 package com.flab.recipebook.common.advice;
 
 import com.flab.recipebook.common.dto.ResponseResult;
+import com.flab.recipebook.recipe.exception.AccessDeniedException;
 import com.flab.recipebook.recipe.exception.RecipeNotFoundException;
 import com.flab.recipebook.user.exception.DuplicateValueException;
+import com.flab.recipebook.user.exception.NotFoundUserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -16,36 +18,45 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
+    //jwt
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ResponseResult> MethodArgumentNotValidException(MethodArgumentNotValidException exception){
-        List<String> errors = bindMultiErrors(exception);
-        ResponseResult responseResult = new ResponseResult(errors);
-
-        return new ResponseEntity<>(responseResult, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(bindMultiErrors(exception), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DuplicateValueException.class)
     protected ResponseEntity<ResponseResult> DuplicateValueException(DuplicateValueException exception){
-        List<String> errors = bindSingleError(exception);
-        ResponseResult responseResult = new ResponseResult(errors);
-        
-        return new ResponseEntity<>(responseResult, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(bindSingleError(exception), HttpStatus.BAD_REQUEST);
     }
 
+    //Recipe
     @ExceptionHandler(RecipeNotFoundException.class)
     protected ResponseEntity<ResponseResult> RecipeNotFoundException(RecipeNotFoundException exception) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(bindSingleError(exception), HttpStatus.NOT_FOUND);
     }
 
-    private List<String> bindMultiErrors(BindException exception){
-        return exception.getBindingResult()
+    @ExceptionHandler(AccessDeniedException.class)
+    protected ResponseEntity<ResponseResult> AccessDeniedException(AccessDeniedException exception) {
+        return new ResponseEntity<>(bindSingleError(exception), HttpStatus.FORBIDDEN);
+    }
+
+    //user
+    @ExceptionHandler(NotFoundUserException.class)
+    protected ResponseEntity<ResponseResult> NotFoundUserException(NotFoundUserException exception) {
+        return new ResponseEntity<>(bindSingleError(exception), HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseResult bindMultiErrors(BindException exception){
+        List<String> errors = exception.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(e -> e.getDefaultMessage())
                 .collect(Collectors.toList());
+        return new ResponseResult(errors);
     }
 
-    private List<String> bindSingleError(RuntimeException exception){
-        return Arrays.asList(exception.getMessage());
+    private ResponseResult bindSingleError(RuntimeException exception){
+        List<String> errors = Arrays.asList(exception.getMessage());
+        return new ResponseResult(errors);
     }
 }
